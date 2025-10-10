bdb0c753efd9b1fa68b570cdc8d7d47f68ec647f
Surya-majhi32038 <suryamajhi32038@gmail.com>
2025-10-10 12:27:27 +0530

    add rules for any problems in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,30 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [
+//       "node_modules/**",
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,9 +36,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Override the rule for this specific configuration file
+    files: ["eslint.config.js", "eslint.config.mjs"], 
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off", // <--- THIS is the rule to turn off
+    }
+  },
   {
     ignores: [
       "node_modules/**",
@@ -24,3 +57,4 @@ const eslintConfig = [
 ];
 
 export default eslintConfig;
+ 
